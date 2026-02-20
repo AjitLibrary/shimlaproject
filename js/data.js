2a8b4e983de7a99c9319f2ea3116cde62e2debf9
Ajit <68845280+AjitLibrary@users.noreply.github.com>
2026-02-20 11:47:34 +0530

    Add data.js with historical site information

---
+++ js/data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { name:"Christ Church", type:"govt", year:1857, lat:31.1048, lng:77.1734 },
+  { name:"Gaiety Theatre", type:"library", year:1887, lat:31.1045, lng:77.1732 },
+  { name:"Peterhoff", type:"hotel", year:1860, lat:31.1040, lng:77.1526 },
+  { name:"Town Hall", type:"govt", year:1910, lat:31.1039, lng:77.1728 },
+  { name:"Railway Board Building", type:"govt", year:1896, lat:31.1035, lng:77.1760 }
+];
